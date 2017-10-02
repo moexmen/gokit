f58702269ce1e5695fc41145cdc2af36dac45783
abeltay <abel.tay@gmail.com>
2017-10-02 09:38:44 +0800

    Reduce use of one channel (#3)

--- web/server.go
+++ web/server.go
@@ -24,12 +24,12 @@ func (s *Server) ListenAndServe() error {
 		Handler: s.Handler,
 	}
 
-	var err error
-	done := make(chan struct{})
+	// We want to use an error channel to block and receive the error
+	serverErr := make(chan error, 1)
+
 	// Start the listener.
 	go func() {
-		err = server.ListenAndServe()
-		close(done)
+		serverErr <- server.ListenAndServe()
 	}()
 
 	// Listen for an interrupt signal from the OS. Use a buffered
@@ -37,20 +37,21 @@ func (s *Server) ListenAndServe() error {
 	osSignals := make(chan os.Signal, 1)
 	signal.Notify(osSignals, os.Interrupt, syscall.SIGTERM)
 
-	var sdErr error
 	// If server.ListenAndServe() cannot startup due to errors such as "port in use",
 	// it will return error and get stuck waiting for osSignals.
 	// This is not ideal because if a server doesn't start, we want to log the error and exit.
 	// Unfortunately, we can't select on a waitgroup.
 	// The done channel and select statement is used to handle the above case.
 	select {
-	case <-done:
+	case err := <-serverErr:
+		return err
 	case <-osSignals:
 		ctx, cancel := context.WithTimeout(context.Background(), s.Timeout)
 		defer cancel()
 
 		// Attempt the graceful shutdown by closing the listener and
 		// completing all inflight requests.
+		var sdErr error
 		if err := server.Shutdown(ctx); err != nil {
 			sdErr = err
 			// Looks like we timedout on the graceful shutdown. Kill it hard.
@@ -58,12 +59,10 @@ func (s *Server) ListenAndServe() error {
 				sdErr = err
 			}
 		}
+		if sdErr != nil {
+			return sdErr
+		}
+		// If we're in this select block, we can safely collect the error from this channel
+		return <-serverErr
 	}
-
-	<-done
-
-	if sdErr != nil {
-		return sdErr
-	}
-	return err
 }
